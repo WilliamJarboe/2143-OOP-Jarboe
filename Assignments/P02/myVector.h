6bdbf47752339ae5944cc5609427a2ca602482c7
WilliamJarboe <42993548+WilliamJarboe@users.noreply.github.com>
2018-09-28 16:03:14 -0500

    Create myVector.h

---
+++ Assignments/P02/myVector.h
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+class myVector {
+private:
+	//reference variables
+	int *theVector;
+	int nextEmpty = 0;
+	int arraySize;
+	int originalSize;
+
+	//dynamically allocating functions that work on push
+	//and pop as needed
+	int* expandArray();
+	int* shrinkArray();
+
+public:
+	//constructor
+	myVector(int size);
+	//publically accessible methods
+	void printExisting();
+	void printWhole();
+	void push(int val);
+	void push(string val);//troll
+	int pop();
+	int getCount();
+	int getMemory();
+	int operator [](int i);
+	int operator <<(int i);
+	int operator +(int i);
+	int operator -(int i);
+	int operator *(int i);
+	int operator /(int i);
+	int operator ==(int i);
+	int operator =(int i);
+	int push(myVector othervec);
+};
