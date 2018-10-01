5f3da70ef96c4004d28e4237dd1401945534f4f7
WilliamJarboe <42993548+WilliamJarboe@users.noreply.github.com>
2018-10-01 00:12:22 -0500

    Update myVector.h

--- Assignments/P02/myVector.h
+++ Assignments/P02/myVector.h
@@ -4,34 +4,38 @@ using namespace std;
 class myVector {
 private:
 	//reference variables
-	int *theVector;
-	int nextEmpty = 0;
-	int arraySize;
-	int originalSize;
+	int				*theVector;
+	int				nextEmpty			= 0;
+	int				arraySize;
+	int				originalSize;
 
-	//dynamically allocating functions that work on push
+	//dynamically allocating functions that work on push_back
 	//and pop as needed
-	int* expandArray();
-	int* shrinkArray();
+	int*			expandArray			();
+	int*			shrinkArray			();
+
+	void			silentpop			();
 
 public:
 	//constructor
-	myVector(int size);
+	myVector							(int size);
 	//publically accessible methods
-	void printExisting();
-	void printWhole();
-	void push(int val);
-	void push(string val);//troll
-	int pop();
-	int getCount();
-	int getMemory();
-	int operator [](int i);
-	int operator <<(int i);
-	int operator +(int i);
-	int operator -(int i);
-	int operator *(int i);
-	int operator /(int i);
-	int operator ==(int i);
-	int operator =(int i);
-	int push(myVector othervec);
+	void			printExisting		();
+	void			printWhole			();
+	void			push_back			(int val);
+	void			push_back			(string val);//troll
+	int				pop					();
+	int				getCount			();
+	int				getMemory			();
+	int				operator		[]	(int i);
+	friend ostream& operator		<<	(ostream& os, myVector& vec);
+	myVector		operator		+	(myVector v);
+	myVector		operator		-	(myVector v);
+	myVector		operator		*	(myVector v);
+	myVector		operator		*	(int v);
+	myVector		operator		/	(myVector v);
+	myVector		operator		/	(int v);
+	myVector		operator		==	(myVector v);
+	void			operator		=	(myVector i);
+	void			push_back			(int v[], int n);
 };
